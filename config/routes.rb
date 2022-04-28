cabbb39aa4d5d8ee2a82746d785e8b210f155b0d
Vitor Souza <macacofinn@yahoo.com>
2022-04-27 22:01:20 -0400

    upt(model, view): new, edit, index views, routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
+  resources :users, execept: [:destroy, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
 end
